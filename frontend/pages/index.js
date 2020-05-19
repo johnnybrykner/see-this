f606fc065ff64aecbc7b229f3a98414b52556a64
Vít Brykner <johnny.brykner@gmail.com>
2020-05-19 14:37:43 +0200

    Set up monorepo
    With two subfolders for the respective project parts, each with up
    to date dependencies.

---
+++ frontend/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>What are we watching today?</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hi!</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
